4b6f084311154e0961937fda7ced70386c0e7062
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-03-31 21:14:00 +0300

    SpringBoot - Make a form for contact submission.

--- SpringBoot/src/main/java/present/programmer/spring/boot/sandbox/Main.java
+++ SpringBoot/src/main/java/present/programmer/spring/boot/sandbox/Main.java
@@ -1,13 +1,9 @@
 package present.programmer.spring.boot.sandbox;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
 public class Main {
 
 	public static void main(String[] args) {
