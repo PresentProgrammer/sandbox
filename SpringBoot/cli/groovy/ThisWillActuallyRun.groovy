5724dfeda3a29b07fdeffba2a2f63ae3f916d7f4
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-04-02 23:58:50 +0300

    SpringBoot: - Clean up HelloWorld Groovy script; - Add JavaDoc to it.

--- SpringBoot/cli/groovy/ThisWillActuallyRun.groovy
+++ SpringBoot/cli/groovy/ThisWillActuallyRun.groovy
@@ -1,13 +1,14 @@
-package present.programmer.spring.boot.sandbox.cli
-
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
+/**
+ * This Groovy script can be run by Spring Boot CLI.
+ * See https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#getting-started-installing-the-cli
+ * ... or Google "Spring Boot CLI".
+ */
 @RestController
+@SuppressWarnings("GroovyUnusedDeclaration")
 class ThisWillActuallyRun {
 
     @RequestMapping("/")
-    String home() {
+    static String home() {
         "Hello World!"
     }
 }
