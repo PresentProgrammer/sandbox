2de367f8a3b572a2ac7689fc5ac2d41cf95862c8
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-04-22 22:26:30 +0300

    - Solve dynamic connectivity task.

---
+++ Algorithms/src/main/java/present/programmer/algorithms/sandbox/union/PrimitiveLinkedList.java
@@ -0,0 +1,95 @@
+package present.programmer.algorithms.sandbox.union;
+
+public class PrimitiveLinkedList {
+
+    private boolean[] present;
+    private int[] successors;
+    private int[] predecessors;
+
+    public PrimitiveLinkedList(final int n) {
+        initializePresent(n);
+        initializeSuccessors(n);
+        initializePredecessors(n);
+    }
+
+    /**
+     * Takes constant time.
+     */
+    public void remove(final int x) {
+        elementMustBePresent(x);
+        if (isFirst(x)) {
+            makeSuccessorFirst(x);
+        } else if (isLast(x)) {
+            makePredecessorFirst(x);
+        } else {
+            removeMiddleElement(x);
+        }
+        present[x] = false;
+    }
+
+    /**
+     * Takes constant time.
+     */
+    public int successor(final int x) {
+        elementMustBePresent(x);
+        return successors[x];
+    }
+
+    //
+    // Auxiliary Methods
+    //
+
+    private void initializePresent(final int n) {
+        present = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            present[i] = true;
+        }
+    }
+
+    private void initializePredecessors(final int n) {
+        predecessors = new int[n];
+        for (int i = n - 1; i > 0; i--) {
+            predecessors[i] = i - 1;
+        }
+        predecessors[0] = 0;
+    }
+
+    private void initializeSuccessors(final int n) {
+        successors = new int[n];
+        for (int i = 0; i < (n - 1); i++) {
+            successors[i] = i + 1;
+        }
+        successors[n - 1] = n - 1;
+    }
+
+    private void elementMustBePresent(final int x) {
+        if (!present[x]) {
+            throw new IllegalArgumentException("element " + x + " has been removed earlier");
+        }
+    }
+
+    private boolean isFirst(final int x) {
+        return predecessors[x] == x;
+    }
+
+    private void makeSuccessorFirst(final int x) {
+        final int xSuccessor = successors[x];
+        predecessors[xSuccessor] = xSuccessor;
+    }
+
+    private boolean isLast(final int x) {
+        return successors[x] == x;
+    }
+
+    private void makePredecessorFirst(final int x) {
+        final int xPredecessor = predecessors[x];
+        successors[xPredecessor] = xPredecessor;
+    }
+
+    private void removeMiddleElement(final int x) {
+        final int xSuccessor = successors[x];
+        final int xPredecessor = predecessors[x];
+        predecessors[xSuccessor] = xPredecessor;
+        successors[xPredecessor] = xSuccessor;
+    }
+}
---
+++ Algorithms/src/test/java/present/programmer/algorithms/sandbox/union/PrimitiveLinkedListTest.java
@@ -0,0 +1,73 @@
+package present.programmer.algorithms.sandbox.union;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrimitiveLinkedListTest {
+
+    private static final int N = 10;
+
+    @Test
+    public void withoutRemoval_successorsAreReturnedCorrectly() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        for (int i = 0; i < N - 2; i++) {
+            assertEquals(i + 1, testedObject.successor(i));
+        }
+        assertEquals(N - 1, testedObject.successor(N - 1));
+    }
+
+    @Test
+    public void removeMiddleElements_successorsAreReturnedCorrectly() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(1);
+        testedObject.remove(2);
+        testedObject.remove(4);
+        testedObject.remove(N - 1);
+        assertEquals(3, testedObject.successor(0));
+        assertEquals(5, testedObject.successor(3));
+        assertEquals(N - 2, testedObject.successor(N - 2));
+    }
+
+    @Test
+    public void removeFirstElement_successorsAreReturnedCorrectly() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(0);
+        assertEquals(2, testedObject.successor(1));
+    }
+
+    @Test
+    public void removeLastElement_successorOfbeforeLastElement_willBeTheSameElement() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(N - 1);
+        assertEquals(N - 2, testedObject.successor(N - 2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeMiddleElementMultipleTimes_exceptionIsThrown() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(2);
+        testedObject.remove(2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeFirstElementMultipleTimes_exceptionIsThrown() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(0);
+        testedObject.remove(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeLastElementMultipleTimes_exceptionIsThrown() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(N - 1);
+        testedObject.remove(N - 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void queryForSuccessorOfRemovedElement_exceptionIsThrown() {
+        final PrimitiveLinkedList testedObject = new PrimitiveLinkedList(N);
+        testedObject.remove(N - 2);
+        testedObject.successor(N - 2);
+    }
+}
