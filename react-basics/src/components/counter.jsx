becc9e6703e5e40002d38e3747eba5f1cc7651b0
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-08-12 19:16:49 +0300

    - Handling events; - Changing state.

--- react-basics/src/components/counter.jsx
+++ react-basics/src/components/counter.jsx
@@ -2,27 +2,42 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        tags: [ 'tag1', 'tag2', 'tag3' ]
+        count: 0
     };
 
-    renderTags() {
-        if (this.state.tags.length === 0)
-            return <p>There are no tags!</p>;
-        else
-            return (
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
-            );
-    }
+    /**
+     * In arrow function, 'this' resolves to lexically enclosing environment.
+     * Particularly in this case, 'this' will reverence this object, as in Java.
+     */
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    // Alternative way is to use constructor and bind the function.
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
     render() {
         return (
             <div>
-                { this.state.tags.length > 0 && <p>Thanks for having some tags!</p> }
-                { this.renderTags() }
+                <span className={this.badgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm"
+                        onClick={this.handleIncrement}>Increment</button>
             </div>
         );
+    }
+
+    badgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
