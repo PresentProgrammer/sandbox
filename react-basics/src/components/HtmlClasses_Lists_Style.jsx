70b8532709b47fbf4086cb1c1c15826a625ddf58
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-08-12 17:05:00 +0300

    - Separate class about HTML classes, lists and styles.

---
+++ react-basics/src/components/HtmlClasses_Lists_Style.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+/**
+ * This class is not used in project anymore, but is kept to show how to:
+ *  - Dynamically render HTML classes;
+ *  - Render lists;
+ *  - Apply styles.
+ */
+class Counter extends Component {
+    state = {
+        count: 1,
+        tags: [ 'tag1', 'tag2', 'tag3' ]
+    };
+
+    render() {
+        return (
+            <div>
+                <span className={this.badgeClasses()}>{this.formatCount()}</span>
+                <button style={{ fontSize: 30 }} className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+                </ul>
+            </div>
+        );
+    }
+
+    badgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+
+export default Counter;
