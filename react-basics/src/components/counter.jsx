3b87aeb64f3b864eb20addc1503685bc69a814cc
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-08-12 16:50:45 +0300

    - Dynamic HTML class.

--- react-basics/src/components/counter.jsx
+++ react-basics/src/components/counter.jsx
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
-    };
-
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
+        count: 1
     };
 
     render() {
         return (
             <div>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button style={{fontSize: 20}} className="btn btn-secondary btn-sm">Increment</button>
+                <span className={this.badgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
+    }
+
+    badgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
