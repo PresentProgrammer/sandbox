2a5f56d7873e9f1636ab7f84ddb070de396f8bf8
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-08-12 15:15:10 +0300

    - Applying styles.

--- react-basics/src/components/counter.jsx
+++ react-basics/src/components/counter.jsx
@@ -5,11 +5,16 @@ class Counter extends Component {
         count: 0
     };
 
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    };
+
     render() {
         return (
             <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button style={{fontSize: 20}} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
