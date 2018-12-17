90fb80047e58d4aaa6f1c54abb368910c2863a2c
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2018-12-17 05:59:36 +0200

    Improve TwoSum solution

--- Practice/src/main/java/present/programmer/practice/leetcode/algorithms/TwoSum.java
+++ Practice/src/main/java/present/programmer/practice/leetcode/algorithms/TwoSum.java
@@ -1,18 +1,23 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        final Map<Integer, Integer> map = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-                    return new int[] { i, j };
-                }
-            }
+        final Integer current = nums[i];
+            final Integer needed = target - current;
+        if (map.get(needed) == null) {
+            map.put(current, i);
+        } else {
+            return new int[] {map.get(needed), i};
         }
-	return new int[] {};
+        }
+        return new int[] {};        
     }
     
     public static void main(final String... args) {
-        System.out.println(Arrays.toString(new TwoSum().twoSum(new int[] { 2, 7, 11, 15 }, 9)));
+    System.out.println(Arrays.toString(new TwoSum().twoSum(new int[] { 2, 7, 11, 15 }, 9)));
     }
 }
