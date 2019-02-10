a73bf1aaa35f7af5631e2fd08c2d4c85792169e9
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-02-10 14:57:11 +0200

    Add ProblemTemplate class

---
+++ Practice/src/main/java/ProblemTemplate.java
@@ -0,0 +1,15 @@
+/**
+ * Problem #x
+ * Time complexity: O()
+ * Space complexity: O()
+ **/
+public class ProblemTemplate {
+    
+    public String problemMainMethod() {
+		return "";
+    }
+    
+    public static void main(final String[] args) {
+        System.out.println(new ProblemTemplate().problemMainMethod());
+	}
+}
