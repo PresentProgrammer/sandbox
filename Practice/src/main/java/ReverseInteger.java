6efa45cbffac47582f9942584ec505e40d365c19
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-02-10 17:03:04 +0200

    - Solve ReverseInteger with Java 8 Math.addExact and Math.multiplyExact. - Solve ReverseInteger with approach proposed on LeetCode.

--- Practice/src/main/java/ReverseInteger.java
+++ Practice/src/main/java/ReverseInteger.java
@@ -4,8 +4,38 @@
  * Space complexity: O(1)
  **/
 public class ReverseInteger {
+	
+	private static final int MIN_OVERFLOW_BORDER = Integer.MIN_VALUE / 10;
+	private static final int MAX_OVERFLOW_BORDER = Integer.MAX_VALUE / 10;
+
+	public int reverse(int x) {
+		int result = 0;
+		while (x != 0) {
+			int pop = x % 10;
+			if (result < MIN_OVERFLOW_BORDER || result == MIN_OVERFLOW_BORDER && pop < -8
+					|| result > MAX_OVERFLOW_BORDER || result == MAX_OVERFLOW_BORDER && pop > 7) {
+				return 0;
+			}
+			result = result * 10 + pop;
+			x /= 10;
+		}
+		return result;
+	}
     
-    public int reverse(int x) {
+	public int reverse_usingJava8ExactAddAndMultiply(int x) {
+		try {
+			int result = 0;
+			while (x != 0) {
+				result = Math.addExact(Math.multiplyExact(result, 10), x % 10);
+				x /= 10;
+			}
+			return result;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+	
+    public int reverse_checkOverflowByDoingReverseOperations(int x) {
 		int result = 0;
 		while (x != 0) {
 			int nextDigit = x % 10;
@@ -18,7 +48,7 @@ public class ReverseInteger {
 			}
 		}
 		return result;
-    }	
+    }
     
     public static void main(final String[] args) {
         System.out.println(new ReverseInteger().reverse(123));
