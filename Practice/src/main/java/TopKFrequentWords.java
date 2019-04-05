2aa3c1fa10b517e94efd01fade81594e7be8352f
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-05 20:25:29 +0300

    - Improve solution

--- Practice/src/main/java/TopKFrequentWords.java
+++ Practice/src/main/java/TopKFrequentWords.java
@@ -1,9 +1,8 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Problem #692
- * Time complexity: O(n log n)
+ * Time complexity: O(n log k)
  * Space complexity: O(n)
  **/
 public class TopKFrequentWords {
@@ -11,44 +10,29 @@ public class TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
 		final Map<String, Integer> map = new HashMap<>();
 		for (final String word : words) {
-		    map.compute(word, (unusedKey, v) -> v == null ? 1 : v + 1);
-        }
-		final Set<WordCount> wordCounts = new TreeSet<>();
-		for (final Map.Entry<String, Integer> entry : map.entrySet()) {
-		    wordCounts.add(new WordCount(entry.getKey(), entry.getValue()));
-        }
-		return wordCounts.stream()
-                .limit(k)
-                .map(WordCount::getWord)
-                .collect(Collectors.toList());
-    }
-
-    private static class WordCount implements Comparable<WordCount> {
-
-        private String word;
-        private int count;
-
-        private WordCount(String word, int count) {
-            this.word = word;
-            this.count = count;
+		    map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        private String getWord() {
-            return this.word;
-        }
-
-        @Override
-        public int compareTo(final WordCount other) {
-            if (this.count > other.count) {
-                return -1;
-            } else if (this.count < other.count) {
-                return 1;
-            } else {
-                return this.word.compareTo(other.word);
+		final PriorityQueue<String> heap = new PriorityQueue<>((left, right) -> {
+            final Integer leftCount = map.get(left);
+            final Integer rightCount = map.get(right);
+            return leftCount.equals(rightCount) ? -left.compareTo(right) : leftCount - rightCount;
+        });
+		for (final String word : map.keySet()) {
+		    heap.offer(word);
+		    if (heap.size() > k) {
+		        heap.poll();
             }
         }
+
+		final List<String> result = new ArrayList<>(k);
+		while (heap.size() > 0) {
+		    result.add(heap.poll());
+        }
+		Collections.reverse(result);
+		return result;
     }
-    
+
     public static void main(final String[] args) {
         System.out.println("[\"i\", \"love\"] == " + new TopKFrequentWords().topKFrequent(
                 new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2));
