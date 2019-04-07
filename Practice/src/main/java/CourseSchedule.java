a3fe66c33d7ccd5f6581b6435dbedf5b95af78cc
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-07 19:35:19 +0300

    Solve CourseSchedule with Kahn's algorithm.

--- Practice/src/main/java/CourseSchedule.java
+++ Practice/src/main/java/CourseSchedule.java
@@ -1,56 +1,50 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Queue;
 
 /**
  * Problem #207
- * Time complexity: O(k), where k — number of vertices.
+ * Time complexity: O(n + k)
  * Space complexity: O(n + k), where n — number of vertices, k — number of edges.
  **/
 public class CourseSchedule {
 
     private static final float HASH_MAP_DEFAULT_LOAD_FACTOR = 0.75F;
 
-    private Map<Integer, List<Integer>> adjacencyLists;
-    private Set<Integer> visitedVertices;
-
+    /**
+     * Solution applies Kahn's algorithm to determine whether there is a cycle in the graph.
+     * See: https://en.wikipedia.org/wiki/Topological_sorting#Algorithms
+     */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        adjacencyLists = new HashMap<>((int) (numCourses / HASH_MAP_DEFAULT_LOAD_FACTOR + 1));
+        final Map<Integer, List<Integer>> adjacencyLists = new HashMap<>((int) (numCourses / HASH_MAP_DEFAULT_LOAD_FACTOR + 1));
+        final int[] inDegrees = new int[numCourses];
         for (final int[] edge : prerequisites) {
             adjacencyLists.compute(edge[0], (k, v) -> nullableWithElement(v, edge[1]));
+            inDegrees[edge[1]]++;
         }
 
-        visitedVertices = new HashSet<>();
+        final Queue<Integer> s = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegrees[i] == 0) {
+                s.offer(i);
+            }
+        }
 
-        for (final Integer start : adjacencyLists.keySet()) {
-            if (!visitedVertices.contains(start)) {
-                final Set<Integer> path = new HashSet<>();
-                if (findCycle(start, path)) {
-                    return false;
+        int visitedCount = 0;
+        while (!s.isEmpty()) {
+            final int currVertex = s.poll();
+            visitedCount++;
+            for (final int to : adjacencyLists.getOrDefault(currVertex, Collections.emptyList())) {
+                if (--inDegrees[to] == 0) {
+                    s.offer(to);
                 }
             }
         }
-        return true;
-    }
-
-    private boolean findCycle(Integer currVertex, Set<Integer> path) {
-        if (path.contains(currVertex)) {
-            return true;
-        } else if (!visitedVertices.contains(currVertex)) {
-            visitedVertices.add(currVertex);
-            path.add(currVertex);
-            for (final Integer next : adjacencyLists.getOrDefault(currVertex, Collections.emptyList())) {
-                if (findCycle(next, path)) {
-                    return true;
-                }
-            }
-            path.remove(currVertex);
-        }
-        return false;
+        return visitedCount == numCourses;
     }
 
     private static List<Integer> nullableWithElement(List<Integer> nullableList, Integer elem) {
