11e4c39774d8454fc44934ad7e1523943824068e
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-07 10:15:28 +0300

    - Add SomeTests for experiments.

---
+++ Practice/src/test/java/SomeTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class SomeTests {
+
+    @Test
+    public void m() {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
