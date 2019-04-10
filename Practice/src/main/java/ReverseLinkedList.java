a6da08545ff8435140fd38241e5b0d1f9efc71e5
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-10 23:46:40 +0300

    Improve ReverseLinkedList space to O(1) by removing recursion.

--- Practice/src/main/java/ReverseLinkedList.java
+++ Practice/src/main/java/ReverseLinkedList.java
@@ -1,11 +1,27 @@
 /**
  * Problem #206
  * Time complexity: O(n)
- * Space complexity: O(n)
+ * Space complexity: O(1)
  **/
 public class ReverseLinkedList {
 
     public ListNode reverseList(final ListNode head) {
+        if (head == null) {
+            return null;
+        } else {
+            ListNode prev = head, curr = prev.next, next;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head.next = null;
+            return prev;
+        }
+    }
+
+    public ListNode m(final ListNode head) {
 		if (head == null) {
 		    return null;
         } else if (head.next == null) {
