5cff505b2ba8923b4c23244a82e072a0655aaea2
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-13 12:32:07 +0300

    Solve WordLadder with bidirectional BFS.

--- Practice/src/main/java/WordLadder.java
+++ Practice/src/main/java/WordLadder.java
@@ -4,11 +4,9 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 
 import static java.util.Arrays.asList;
 
@@ -19,7 +17,13 @@ import static java.util.Arrays.asList;
  **/
 public class WordLadder {
 
+	private static Map<String, String[]> GENERIC_WORDS = new HashMap<>();
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    	if (!wordList.contains(endWord)) {
+    		return 0;
+		}
+
 		final Map<String, List<String>> allComboDict = new HashMap<>();
 		for (final String word : wordList) {
 		    for (final String genericWord : genericWordsOf(word)) {
@@ -31,35 +35,47 @@ public class WordLadder {
             }
         }
 
-		final Set<String> visited = new HashSet<>();
-		final Queue<Pair<String, Integer>> queue = new ArrayDeque<>();
-		queue.offer(new Pair<>(beginWord, 1));
-		while (!queue.isEmpty()) {
-		    final Pair<String, Integer> curr = queue.poll();
-		    final String currWord = curr.getKey();
-		    final Integer currDepth = curr.getValue();
-		    for (final String currGenericWord : genericWordsOf(currWord)) {
-		        if (!visited.contains(currGenericWord)) {
-		            visited.add(currGenericWord);
-		            for (final String nextWord : allComboDict.getOrDefault(currGenericWord, Collections.emptyList())) {
-		                if (nextWord.equals(endWord)) {
-		                    return currDepth + 1;
-                        } else {
-		                    queue.offer(new Pair<>(nextWord, currDepth + 1));
-                        }
-                    }
-                }
-            }
-        }
+		final Map<String, Integer> beginVisited = new HashMap<>();
+		beginVisited.put(beginWord, 1);
+		final Map<String, Integer> endVisited = new HashMap<>();
+		endVisited.put(endWord, 1);
+		final Queue<Pair<String, Integer>> beginQueue = new ArrayDeque<>();
+		final Queue<Pair<String, Integer>> endQueue = new ArrayDeque<>();
+		beginQueue.add(new Pair<>(beginWord, 1));
+		endQueue.add(new Pair<>(endWord, 1));
+		Map<String, Integer> activeVisited = endVisited;
+		Queue<Pair<String, Integer>> activeQueue = endQueue;
+		while (!beginQueue.isEmpty() && !endQueue.isEmpty()) {
+			activeVisited = activeVisited == beginVisited ? endVisited : beginVisited;
+			activeQueue = activeQueue == beginQueue ? endQueue : beginQueue;
+			final Pair<String, Integer> curr = activeQueue.poll();
+			final String currWord = curr.getKey();
+			final Integer currLength = curr.getValue();
+			for (final String currGenericWord : genericWordsOf(currWord)) {
+				for (final String nextWord : allComboDict.getOrDefault(currGenericWord, Collections.emptyList())) {
+					if (!activeVisited.containsKey(nextWord)) {
+						final Map<String, Integer> inactiveVisited = activeVisited == beginVisited ? endVisited : beginVisited;
+						if ((inactiveVisited).containsKey(nextWord)) {
+							return currLength + inactiveVisited.get(nextWord);
+						} else {
+							activeVisited.put(nextWord, currLength + 1);
+							activeQueue.offer(new Pair<>(nextWord, currLength + 1));
+						}
+					}
+				}
+			}
+		}
 		return 0;
     }
 
     private static String[] genericWordsOf(final String word) {
-        final String[] genericWords = new String[word.length()];
-        for (int i = 0; i < word.length(); i++) {
-            genericWords[i] = word.substring(0, i) + "*" + word.substring(i + 1);
-        }
-        return genericWords;
+    	return GENERIC_WORDS.computeIfAbsent(word, (w) -> {
+			final String[] genericWords = new String[w.length()];
+			for (int i = 0; i < w.length(); i++) {
+				genericWords[i] = w.substring(0, i) + "*" + w.substring(i + 1);
+			}
+			return genericWords;
+		});
     }
 
     public static void main(final String[] args) {
