b3c3c54093263e978555a7a7a46c7ea4a54036b0
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-14 21:10:37 +0300

    Solve CountAndSay.

---
+++ Practice/src/main/java/CountAndSay.java
@@ -0,0 +1,37 @@
+/**
+ * Problem #38
+ * Time complexity: O(?)
+ * Space complexity: O(?)
+ **/
+public class CountAndSay {
+
+    public String countAndSay(int n) {
+		return n == 1 ? "1" : transform(countAndSay(n - 1));
+    }
+
+    private static String transform(final String n) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        int pos = 0;
+        while (pos < n.length()) {
+            final char number = n.charAt(pos);
+            pos++;
+            int count = 1;
+            while (pos < n.length() && n.charAt(pos) == number) {
+                count++;
+                pos++;
+            }
+            stringBuilder
+                    .append(count)
+                    .append(number);
+        }
+        return stringBuilder.toString();
+    }
+    
+    public static void main(final String[] args) {
+        System.out.println("1 == " + new CountAndSay().countAndSay(1));
+        System.out.println("11 == " + new CountAndSay().countAndSay(2));
+        System.out.println("21 == " + new CountAndSay().countAndSay(3));
+        System.out.println("1211 == " + new CountAndSay().countAndSay(4));
+        System.out.println("? == " + new CountAndSay().countAndSay(30));
+	}
+}
