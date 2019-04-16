34c4a19fbcec0de4a2c0945272ccfea748c5b2bc
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-16 22:39:27 +0300

    Solve SqrtX.

---
+++ Practice/src/main/java/SqrtX.java
@@ -0,0 +1,14 @@
+/**
+ * Problem #69
+ * Time complexity: O(?)
+ * Space complexity: O(1)
+ **/
+public class SqrtX {
+
+    public int mySqrt(int x) {
+		return (int) Math.sqrt(x);
+    }
+    
+    public static void main(final String[] args) {
+	}
+}
