e9fee5a093274ade4140f9f13646b066773aec5a
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-16 23:13:54 +0300

    Solve SqrtX with binary search.

--- Practice/src/main/java/SqrtX.java
+++ Practice/src/main/java/SqrtX.java
@@ -6,7 +6,22 @@
 public class SqrtX {
 
     public int mySqrt(int x) {
-		return (int) Math.sqrt(x);
+		if (x == 0) {
+		    return x;
+        }
+		int left = 1, right = x;
+		while (left <= right) {
+		    final int mid = (left + right) / 2;
+		    final int divRes = x / mid;
+		    if (divRes == mid || divRes == mid - 1) {
+		        return divRes;
+            } else if (divRes > mid) {
+		        left = mid + 1;
+            } else {
+		        right = mid - 1;
+            }
+        }
+		return -1;
     }
     
     public static void main(final String[] args) {
