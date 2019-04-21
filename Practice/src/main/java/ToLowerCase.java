3fe101c25fb0b6370ab787036e04d83d85d88ad6
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-21 12:14:39 +0300

    Solve ToLowerCase.

---
+++ Practice/src/main/java/ToLowerCase.java
@@ -0,0 +1,33 @@
+/**
+ * Problem #709
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ **/
+public class ToLowerCase {
+
+    private static final char[] CHAR_MAP;
+
+    static {
+        CHAR_MAP = new char[256];
+        for (char i = 0; i < 256; i++) {
+            CHAR_MAP[i] = i;
+        }
+        for (int i = 65; i < 91; i++) {
+            CHAR_MAP[i] = (char) (i + 32);
+        }
+    }
+
+    public String toLowerCase(String str) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            builder.append(CHAR_MAP[str.charAt(i)]);
+        }
+        return builder.toString();
+    }
+    
+    public static void main(final String[] args) {
+        System.out.println("hello == " + new ToLowerCase().toLowerCase("Hello"));
+        System.out.println("here == " + new ToLowerCase().toLowerCase("here"));
+        System.out.println("LOVELY == " + new ToLowerCase().toLowerCase("LOVELY"));
+	}
+}
--- Practice/src/test/java/SomeTests.java
+++ Practice/src/test/java/SomeTests.java
@@ -4,7 +4,9 @@ public class SomeTests {
 
     @Test
     public void m() {
-        System.out.println((int) 'z');
+        System.out.println((int) 'A');
         System.out.println((int) 'Z');
+        System.out.println((int) 'a');
+        System.out.println((int) 'z');
     }
 }
