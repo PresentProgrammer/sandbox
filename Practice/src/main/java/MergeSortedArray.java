430569c9fc28de68c1e0ea7a8d9c5c3ff577a016
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-25 23:10:34 +0300

    Improve MergeSortedArray to be O(1) space.

--- Practice/src/main/java/MergeSortedArray.java
+++ Practice/src/main/java/MergeSortedArray.java
@@ -1,25 +1,19 @@
-import java.util.Arrays;
-
 /**
  * Problem #88
  * Time complexity: O(m + n)
- * Space complexity: O(m)
+ * Space complexity: O(1)
  **/
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-		final int[] nums1Copy = Arrays.copyOf(nums1, m);
-        int p1 = 0;
-		int p2 = 0;
-		int p = 0;
-		while (p1 < m && p2 < n) {
-		    nums1[p++] = nums1Copy[p1] <= nums2[p2] ? nums1Copy[p1++] : nums2[p2++];
+        int p1 = m - 1;
+		int p2 = n - 1;
+		int p = m + n - 1;
+		while (p1 >= 0 && p2 >= 0) {
+		    nums1[p--] = nums1[p1] >= nums2[p2] ? nums1[p1--] : nums2[p2--];
         }
-		while (p1 < m) {
-            nums1[p++] = nums1Copy[p1++];
-        }
-		while (p2 < n) {
-            nums1[p++] = nums2[p2++];
+		while (p2 >= 0) {
+            nums1[p--] = nums2[p2--];
         }
     }
     
