36c3324891a49831150cf0b1dadda5e520844202
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-27 00:07:33 +0300

    Solve UniquePaths with O(n - m) mathematically.

--- Practice/src/main/java/UniquePaths.java
+++ Practice/src/main/java/UniquePaths.java
@@ -1,31 +1,27 @@
 /**
  * Problem #62
- * Time complexity: O(n * m)
- * Space complexity: O(n * m)
+ * Time complexity: O(m - n)
+ * Space complexity: O(1)
+ * See mathematical explanation: https://leetcode.com/problems/unique-paths/discuss/22958/Math-solution-O(1)-space
  **/
 public class UniquePaths {
 
     public int uniquePaths(int m, int n) {
-        if (m < 1 || n < 1) {
-            return 0;
+        if (m < n) {
+            m ^= n;
+            n ^= m;
+            m ^= n;
         }
-		final int[][] board = new int[n][m];
-		for (int i = 0; i < n; i++) {
-		    board[i][0] = 1;
+        long res = 1;
+        for (int i = m, j = 1; i <= m + n - 2; i++, j++) {
+            res *= i;
+            res /= j;
         }
-		for (int j = 1; j < m; j++) {
-		    board[0][j] = 1;
-        }
-		for (int i = 1; i < n; i++) {
-		    for (int j = 1; j < m; j++) {
-		        board[i][j] = board[i - 1][j] + board[i][j - 1];
-            }
-        }
-		return board[n - 1][m - 1];
+        return (int) res;
     }
     
     public static void main(final String[] args) {
         System.out.println("3 == " + new UniquePaths().uniquePaths(3, 2));
-        System.out.println("28 == " + new UniquePaths().uniquePaths(7, 3));
+        System.out.println("28 == " + new UniquePaths().uniquePaths(3, 7));
 	}
 }
