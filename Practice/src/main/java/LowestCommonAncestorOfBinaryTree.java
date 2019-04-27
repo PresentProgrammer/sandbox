2f5d4c62c592e09e7b1181d356b91c74cabf5954
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-27 16:06:16 +0300

    Improve LowestCommonAncestorOfBinaryTree not to search branches if result has been found.

--- Practice/src/main/java/LowestCommonAncestorOfBinaryTree.java
+++ Practice/src/main/java/LowestCommonAncestorOfBinaryTree.java
@@ -13,26 +13,31 @@ public class LowestCommonAncestorOfBinaryTree {
         this.lca = null;
         this.p = p;
         this.q = q;
-        findLowestCommonAncestor(root);
+        findLowestCommonAncestor(root, 0);
         return this.lca;
     }
 
-    private int findLowestCommonAncestor(TreeNode curr) {
+    private int findLowestCommonAncestor(TreeNode curr, int alreadyFound) {
         if (curr == null) {
             return 0;
         } else {
             int foundFromCurr = 0;
             if (curr.val == p.val || curr.val == q.val) {
                 foundFromCurr++;
+                if (foundFromCurr + alreadyFound > 1) {
+                    return foundFromCurr;
+                }
             }
-            foundFromCurr += findLowestCommonAncestor(curr.left);
+            foundFromCurr += findLowestCommonAncestor(curr.left, alreadyFound);
             if (foundFromCurr > 1) {
                 if (lca == null) {
                     lca = curr;
                 }
                 return foundFromCurr;
+            } else if (foundFromCurr + alreadyFound > 1) {
+                return foundFromCurr;
             } else {
-                foundFromCurr += findLowestCommonAncestor(curr.right);
+                foundFromCurr += findLowestCommonAncestor(curr.right, alreadyFound);
                 if (foundFromCurr > 1 && lca == null) {
                     lca = curr;
                 }
