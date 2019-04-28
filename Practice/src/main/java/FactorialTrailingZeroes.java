0a377e70c4a5384d29237686f8a7a4beb31ba8a8
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-28 16:26:09 +0300

    Solve FactorialTrailingZeroes.

---
+++ Practice/src/main/java/FactorialTrailingZeroes.java
@@ -0,0 +1,20 @@
+/**
+ * Problem #172
+ * Time complexity: O(log n)
+ * Space complexity: O(1)
+ **/
+public class FactorialTrailingZeroes {
+
+    public int trailingZeroes(int n) {
+		int result = 0;
+		while (n > 0) {
+		    n /= 5;
+		    result += n;
+        }
+		return result;
+    }
+    
+    public static void main(final String[] args) {
+        System.out.println("7 == " + new FactorialTrailingZeroes().trailingZeroes(30));
+	}
+}
