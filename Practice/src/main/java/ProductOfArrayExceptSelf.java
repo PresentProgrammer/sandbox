4642fee1cf44a44921c9a1762abb9eda820ca671
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-28 13:06:51 +0300

    Solve ProductOfArrayExceptSelf with O(1) space.

--- Practice/src/main/java/ProductOfArrayExceptSelf.java
+++ Practice/src/main/java/ProductOfArrayExceptSelf.java
@@ -3,39 +3,51 @@ import java.util.Arrays;
 /**
  * Problem #x
  * Time complexity: O(n log n)
- * Space complexity: O(log n)
+ * Space complexity: O(1)
  **/
 public class ProductOfArrayExceptSelf {
 
-    public int[] productExceptSelf(int[] nums) {
-		final int[] out = new int[nums.length];
+    public int[] productExceptSelf(int[] in) {
+		final int[] out = new int[in.length];
 		Arrays.fill(out, 1);
-        productExceptSelf(nums, out, 0, nums.length - 1);
-        return out;
-    }
+        int left = 0, right = in.length - 1;
+        while (left < in.length) {
+            final int mid = (left + right + 1) / 2;
 
-    private static void productExceptSelf(final int[] in, final int[] out, final int left, final int right) {
-        if (right > left) {
-            final int mid = (left + right) / 2;
             int subResult = out[left];
-            for (int i = left; i <= mid; i++) {
+            for (int i = left; i < mid; i++) {
                 subResult *= in[i];
             }
-            out[mid + 1] = subResult;
+            out[mid] = subResult;
 
             subResult = out[left];
-            for (int i = mid + 1; i <= right; i++) {
+            for (int i = mid; i <= right; i++) {
                 subResult *= in[i];
             }
             out[left] = subResult;
 
-            productExceptSelf(in, out, left, mid);
-            productExceptSelf(in, out, mid + 1, right);
+            if (mid - left > 1) {
+                out[right] = Integer.MAX_VALUE;
+                right = mid - 1;
+            } else if (right > mid) {
+                left = mid;
+            } else {
+                left = right + 1;
+                if (left < in.length) {
+                    right = left + 1;
+                    while (out[right] != Integer.MAX_VALUE) {
+                        right++;
+                    }
+                }
+            }
         }
+        return out;
     }
-    
+
     public static void main(final String[] args) {
         System.out.println("[24, 12, 8, 6] == " + Arrays.toString(
-                new ProductOfArrayExceptSelf().productExceptSelf(new int[]{1,2,3,4})));
+                new ProductOfArrayExceptSelf().productExceptSelf(new int[]{ 1, 2, 3, 4 })));
+        System.out.println("[0, -18, 0] == " + Arrays.toString(
+                new ProductOfArrayExceptSelf().productExceptSelf(new int[]{ 9, 0, -2 })));
 	}
 }
