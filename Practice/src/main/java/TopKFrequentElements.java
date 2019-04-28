f27cbfa84c633f390c7f80ffeab74a1ea5af5f5d
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-28 17:54:12 +0300

    Improve TopKFrequentElements to be O(n) time by not using sort (peeked into Discussion).

--- Practice/src/main/java/TopKFrequentElements.java
+++ Practice/src/main/java/TopKFrequentElements.java
@@ -1,42 +1,41 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 /**
  * Problem #347
- * Time complexity: O(n log k)
+ * Time complexity: O(n)
  * Space complexity: O(n)
  **/
 public class TopKFrequentElements {
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
+    @SuppressWarnings("unchecked")
+	public List<Integer> topKFrequent(int[] nums, int k) {
 		final Map<Integer, Integer> frequencies = new HashMap<>();
 		for (final int num : nums) {
 		    frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
         }
 
-		final PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-		    @Override
-            public int compare(Integer n1, Integer n2) {
-		        return frequencies.get(n1) - frequencies.get(n2);
-            }
-        });
-		for (final Integer element : frequencies.keySet()) {
-			queue.offer(element);
-			if (queue.size() > k) {
-				queue.poll();
-			}
+		final List<Integer>[] buckets = new List[nums.length + 1];
+
+		for (final Map.Entry<Integer, Integer> frequency : frequencies.entrySet()) {
+			List<Integer> bucket = buckets[frequency.getValue()] == null ? new ArrayList<>() : buckets[frequency.getValue()];
+			bucket.add(frequency.getKey());
+			buckets[frequency.getValue()] = bucket;
         }
 
 		final List<Integer> result = new ArrayList<>();
-		while (!queue.isEmpty()) {
-			result.add(queue.poll());
+		for (int i = buckets.length - 1; i >= 0; i--) {
+			if (buckets[i] != null) {
+				for (final Integer element : buckets[i]) {
+					result.add(element);
+					if (result.size() >= k) {
+						return result;
+					}
+				}
+			}
 		}
-		Collections.reverse(result);
 		return result;
     }
 
