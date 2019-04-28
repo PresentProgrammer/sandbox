be86a7f206c68677919f63cf60a61c80518f8073
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-28 13:55:23 +0300

    Solve ProductOfArrayExceptSelf with O(1) space, and O(n) time - simplify previous solution after picking at solution that was proposed on LeetCode.

--- Practice/src/main/java/ProductOfArrayExceptSelf.java
+++ Practice/src/main/java/ProductOfArrayExceptSelf.java
@@ -3,26 +3,20 @@ import java.util.Arrays;
 /**
  * Problem #238
  * Time complexity: O(n)
- * Space complexity: O(n)
+ * Space complexity: O(1)
  **/
 public class ProductOfArrayExceptSelf {
 
     public int[] productExceptSelf(int[] in) {
-		final int[] leftProducts = new int[in.length];
-		leftProducts[0] = 1;
+    	final int[] out = new int[in.length];
+		out[0] = 1;
 		for (int i = 1; i < in.length; i++) {
-		    leftProducts[i] = leftProducts[i - 1] * in[i - 1];
+		    out[i] = out[i - 1] * in[i - 1];
         }
-
-        final int[] rightProducts = new int[in.length];
-		rightProducts[in.length - 1] = 1;
+        int right = in[in.length - 1];
 		for (int i = in.length - 2; i >= 0; i--) {
-		    rightProducts[i] = rightProducts[i + 1] * in[i + 1];
-        }
-
-		final int[] out = new int[in.length];
-		for (int i = 0; i < out.length; i++) {
-		    out[i] = leftProducts[i] * rightProducts[i];
+		    out[i] *= right;
+		    right *= in[i];
         }
 		return out;
     }
