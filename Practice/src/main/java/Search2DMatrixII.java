a1da48da8e0d7a0400883881d26c3a75118d6ba7
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-30 22:23:20 +0300

    Solve Search2DMatrixII with "smart" approach.

--- Practice/src/main/java/Search2DMatrixII.java
+++ Practice/src/main/java/Search2DMatrixII.java
@@ -1,13 +1,7 @@
-import javafx.util.Pair;
-
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-
 /**
  * Problem #240
- * Time complexity: O(?)
- * Space complexity: O(?)
+ * Time complexity: O(n + m)
+ * Space complexity: O(1)
  **/
 public class Search2DMatrixII {
 
@@ -15,67 +9,14 @@ public class Search2DMatrixII {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-        final Queue<Pair<Integer, Integer>> q = new ArrayDeque<>();
-        q.offer(new Pair<>(0, matrix.length - 1));
-        q.offer(new Pair<>(0, matrix[0].length - 1));
-        while (!q.isEmpty()) {
-            final Pair<Integer, Integer> vertRange = q.poll();
-            final int top = vertRange.getKey();
-            final int bottom = vertRange.getValue();
-            final Pair<Integer, Integer> horizRange = q.poll();
-            final int left = horizRange.getKey();
-            final int right = horizRange.getValue();
-            if (top == bottom) {
-                if (horizBinarySearch(matrix, target, top, left, right)) {
-                    return true;
-                }
-            } else if (left == right) {
-                if (vertBinarySearch(matrix, target, left, top, bottom)) {
-                    return true;
-                }
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] < target) {
+                i++;
+            } else if (matrix[i][j] > target) {
+                j--;
             } else {
-                int topNew = top, bottomNew = bottom;
-                int leftNew = left, rightNew = right;
-                while (topNew <= bottomNew && leftNew <= rightNew) {
-                    final int vertMid = (topNew + bottomNew) / 2;
-                    final int horizMid = (leftNew + rightNew) / 2;
-                    if (matrix[vertMid][horizMid] > target) {
-                        bottomNew = vertMid - 1;
-                        rightNew = horizMid - 1;
-                    } else if (matrix[vertMid][horizMid] < target) {
-                        topNew = vertMid + 1;
-                        leftNew = horizMid + 1;
-                    } else {
-                        return true;
-                    }
-                }
-                if (bottomNew >= top && leftNew <= right) {
-                    q.offer(new Pair<>(top, bottomNew));
-                    q.offer(new Pair<>(leftNew, right));
-                }
-                if (topNew <= bottom && rightNew >= left) {
-                    q.offer(new Pair<>(topNew, bottom));
-                    q.offer(new Pair<>(left, rightNew));
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean horizBinarySearch(int[][] matrix, int target, int row, int left, int right) {
-        return Arrays.binarySearch(matrix[row], left, right + 1, target) >= 0;
-    }
-
-    private static boolean vertBinarySearch(int[][] matrix, int target, int col, int _top, int _bottom) {
-        int top = _top, bottom = _bottom;
-        while (top <= bottom) {
-            final int mid = (top + bottom) / 2;
-            if (matrix[mid][col] == target) {
                 return true;
-            } else if (matrix[mid][col] < target) {
-                top = mid + 1;
-            } else {
-                bottom = mid - 1;
             }
         }
         return false;
