aa2627fe9ec969c52de03233f8b78608ab7a7f57
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-01 00:12:01 +0300

    Solve BestTimeToBuyAndSellStock.

---
+++ Practice/src/main/java/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+/**
+ * Problem #121
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ **/
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+		if (prices == null || prices.length < 2) {
+		    return 0;
+        }
+		int min = prices[0], best = 0;
+		for (int i = 1; i < prices.length; i++) {
+		    if (prices[i] < min) {
+		        min = prices[i];
+            } else {
+                best = Math.max(best, prices[i] - min);
+            }
+        }
+		return best;
+    }
+    
+    public static void main(final String[] args) {
+	}
+}
