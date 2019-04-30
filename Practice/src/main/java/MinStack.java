1b87ebcbcdbdfed3da2ee948f8da767206ebb908
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-04-30 22:41:31 +0300

    Solve MinStack.

---
+++ Practice/src/main/java/MinStack.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Problem #155
+ * Time complexity: O(1)
+ * Space complexity: O(n)
+ **/
+public class MinStack {
+
+    private Deque<Integer> elements = new ArrayDeque<>();
+    private Deque<Integer> mins = new ArrayDeque<>();
+
+    public void push(int x) {
+        elements.push(x);
+        if (mins.isEmpty() || mins.peek() >= x) {
+            mins.push(x);
+        }
+    }
+
+    public void pop() {
+        if (mins.peek().equals(elements.pop())) {
+            mins.pop();
+        }
+    }
+
+    public int top() {
+        return elements.peek();
+    }
+
+    public int getMin() {
+        return mins.peek();
+    }
+
+    public static void main(final String[] args) {
+	}
+}
