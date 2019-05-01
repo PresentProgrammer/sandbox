fc92cf8a9281d96b3aa927718029a67a45aa15eb
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-01 21:37:49 +0300

    Solve KthLargestElementInArray with QuickSelect.

--- Practice/src/main/java/KthLargestElementInArray.java
+++ Practice/src/main/java/KthLargestElementInArray.java
@@ -1,21 +1,61 @@
-import java.util.PriorityQueue;
+import java.util.Random;
 
 /**
  * Problem #215
- * Time complexity: O(n log k)
- * Space complexity: O(k)
+ * Time complexity: O(n)
+ * Space complexity: O(1)
  **/
 public class KthLargestElementInArray {
 
     public int findKthLargest(int[] nums, int k) {
-		final PriorityQueue<Integer> Q = new PriorityQueue<>();
-		for (final int num : nums) {
-		    Q.offer(num);
-		    if (Q.size() > k) {
-		        Q.poll();
+		return partitionUntilKLargestFound(shuffle(nums), k - 1);
+    }
+
+    private static int partitionUntilKLargestFound(final int[] nums, final int k) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            final int partitioningElement = nums[left];
+            int i = left + 1, j = right;
+            while (i <= j) {
+                while (i <= j && nums[i] > partitioningElement) {
+                    i++;
+                }
+                while (i <= j && nums[j] < partitioningElement) {
+                    j--;
+                }
+                if (i < j) {
+                    swap(nums, i, j);
+                    i++;
+                    j--;
+                } else if (i == j) {
+                    j--;
+                }
+            }
+            swap(nums, left, j);
+            if (j == k) {
+                left = j;
+                right = j;
+            } else if (j < k) {
+                left = j + 1;
+            } else {
+                right = j - 1;
             }
         }
-		return Q.peek();
+        return nums[left];
+    }
+
+    private static int[] shuffle(final int [] original) {
+        final Random rand = new Random();
+        for (int i = 1; i < original.length; i++) {
+            swap(original, i, rand.nextInt(i + 1));
+        }
+        return original;
+    }
+
+    private static void swap(final int[] nums, final int i, final int j) {
+        final int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
     public static void main(final String[] args) {
