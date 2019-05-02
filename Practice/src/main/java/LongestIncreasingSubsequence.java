394010c424417ebbd6155ed6a53a150e8676725e
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-03 00:02:13 +0300

    Improve solution to O(n log n) time.

--- Practice/src/main/java/LongestIncreasingSubsequence.java
+++ Practice/src/main/java/LongestIncreasingSubsequence.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 /**
  * Problem #300
- * Time complexity: O(n ^ 2)
+ * Time complexity: O(n log n)
  * Space complexity: O(n)
  **/
 public class LongestIncreasingSubsequence {
@@ -12,14 +12,23 @@ public class LongestIncreasingSubsequence {
         final List<Integer> sequences = new ArrayList<>();
         for (int i = nums == null ? -1 : nums.length - 1; i >= 0; i--) {
             final int num = nums[i];
-            int j = sequences.size() - 1;
-            while (j >= 0 && num >= sequences.get(j)) {
-                j--;
+            int left = 0, right = sequences.size() - 1;
+            while (left <= right) {
+                final int mid = (left + right) / 2;
+                final int midValue = sequences.get(mid);
+                if (midValue == num) {
+                    left = mid;
+                    right = mid - 1;
+                } else if (midValue < num) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
-            if (j == sequences.size() - 1) {
+            if (left == sequences.size()) {
                 sequences.add(num);
             } else {
-                sequences.set(j + 1, num);
+                sequences.set(left, num);
             }
         }
         return sequences.size();
