67a43341de2fd19668fa534f6419458c5953d687
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-04 11:00:40 +0300

    Solve RotateArray.

---
+++ Practice/src/main/java/RotateArray.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/**
+ * Problem #189
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ **/
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length < 1 || k == 0) {
+            return;
+        }
+		for (int i = 0; i < greatestCommonDivisor(k, nums.length); i++) {
+		    int j = i, prevTemp = nums[j], temp;
+		    do {
+		        final int next = j + k;
+		        j = next >= nums.length ? next % nums.length : next;
+		        temp = nums[j];
+		        nums[j] = prevTemp;
+		        prevTemp = temp;
+            } while (j != i);
+        }
+    }
+
+    private static int greatestCommonDivisor(final int a, final int b) {
+        return a > b ? gcd(a, b) : gcd(b, a);
+    }
+
+    private static int gcd(final int a, final int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    
+    public static void main(final String[] args) {
+        final int[] nums = {1,2,3,4,5,6};
+        new RotateArray().rotate(nums, 4);
+        System.out.println(Arrays.toString(nums));
+	}
+}
