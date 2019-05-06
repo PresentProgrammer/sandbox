e3775ea9efc5a85be6c5d0c4f60410d2fa01e12b
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-06 19:29:04 +0300

    Solve PalindromeLinkedList.

---
+++ Practice/src/main/java/PalindromeLinkedList.java
@@ -0,0 +1,54 @@
+/**
+ * Problem #234
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ **/
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(final ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        ListNode tortoise = head;
+        ListNode hair = head.next == null ? head : head.next;
+        while (hair.next != null) {
+            hair = hair.next;
+            hair = hair.next == null ? hair : hair.next;
+            tortoise = tortoise.next;
+        }
+
+        ListNode prev = tortoise;
+        ListNode curr = prev.next;
+        ListNode next = curr == null ? null : curr.next;
+        while (curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next == null ? null : next.next;
+        }
+
+        ListNode left = head;
+        ListNode right = hair;
+        while (left != right && left.next != right) {
+            if (left.val == right.val) {
+                left = left.next;
+                right = right.next;
+            } else {
+                return false;
+            }
+        }
+        return left.val == right.val;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(final String[] args) {
+    }
+}
