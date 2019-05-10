5fdc439cd6cfde46738a980c6c11b15e5045f0cc
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-10 20:16:19 +0300

    Solve WiggleSortII.

---
+++ Practice/src/main/java/WiggleSortII.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * Problem #324
+ * Time complexity: O(n log n)
+ * Space complexity: O(n)
+ **/
+public class WiggleSortII {
+
+    public void wiggleSort(int[] nums) {
+        if (nums != null && nums.length > 0) {
+            final int[] auxiliary = Arrays.copyOf(nums, nums.length);
+            Arrays.sort(auxiliary);
+            int lo = (auxiliary.length - 1) / 2, hi = auxiliary.length - 1, i = 0;
+            while (i < nums.length) {
+                nums[i++] = auxiliary[lo--];
+                if (i < nums.length) {
+                    nums[i++] = auxiliary[hi--];
+                }
+            }
+        }
+    }
+    
+    public static void main(final String[] args) {
+	}
+}
