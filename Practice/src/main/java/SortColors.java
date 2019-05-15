b56292bc4fb8edf819223b43752e0150892e5f73
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-15 21:07:01 +0300

    Solve SortColors.

---
+++ Practice/src/main/java/SortColors.java
@@ -0,0 +1,36 @@
+/**
+ * Problem #75
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ **/
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+        if (nums != null) {
+            int left = 0, right = nums.length - 1, i = 0;
+            while (i <= right) {
+                if (nums[i] == 0) {
+                    swap(nums, left, i);
+                    left++;
+                    if (left > i) {
+                        i = left;
+                    }
+                } else if (nums[i] == 2) {
+                    swap(nums, i, right);
+                    right--;
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+    private static void swap(final int[] nums, final int i, final int j) {
+        final int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public static void main(final String[] args) {
+	}
+}
