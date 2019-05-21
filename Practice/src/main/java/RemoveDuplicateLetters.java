89b93255041d47c4572bea09fd758a2a60d59759
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-21 18:06:06 +0300

    Solve with permutations, but time exceeds.

--- Practice/src/main/java/RemoveDuplicateLetters.java
+++ Practice/src/main/java/RemoveDuplicateLetters.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,68 +9,51 @@ import java.util.List;
  **/
 public class RemoveDuplicateLetters {
 
+	private static final String INITIAL_STRING = "{";
     private static final int ALPHABER_SIZE = 26;
+
+    private String input;
+    private String result;
+    private List<Integer>[] charPositions;
 
     @SuppressWarnings("unchecked")
     public String removeDuplicateLetters(final String s) {
-		final List<Integer>[] charPositions = new List[ALPHABER_SIZE];
+    	input = s;
+    	result = INITIAL_STRING;
+		charPositions = new List[ALPHABER_SIZE];
 		for (int i = 0; i < ALPHABER_SIZE; i++) {
 		    charPositions[i] = new ArrayList<>();
         }
 		for (int i = 0; i < s.length(); i++) {
 		    charPositions[s.charAt(i) - 'a'].add(i);
         }
-
-		final int[] selectedIndexes = selectIndexes(
-				new int[ALPHABER_SIZE], charPositions, s.length(), ALPHABER_SIZE);
-		final int[] selectedPositions = new int[ALPHABER_SIZE];
-		for (int i = 0; i < ALPHABER_SIZE; i++) {
-			selectedPositions[i] = selectedIndexes[i] == -1 ? -1 : charPositions[i].get(selectedIndexes[i]);
-		}
-		Arrays.sort(selectedPositions);
-		final StringBuilder strBuilder = new StringBuilder();
-		for (final int pos : selectedPositions) {
-		    if (pos >= 0) {
-                strBuilder.append(s.charAt(pos));
-            }
-        }
-		return strBuilder.toString();
+		permute(0, new ArrayList<>());
+		return result.equals(INITIAL_STRING) ? "" : result;
     }
 
-    private static int[] selectIndexes(final int[] selectedIndexes, final List<Integer>[] charPositions,
-			final int rightBorder, final int rightBorderChar) {
-    	int leftBorder = -1;
-    	int leftAfterRightBorder = rightBorder;
-    	for (int i = 0; i < rightBorderChar; i++) {
-			final List<Integer> positions = charPositions[i];
-			if (positions.isEmpty()) {
-				selectedIndexes[i] = -1;
-			} else {
-				int j = 0;
-				while (j < positions.size() && positions.get(j) <= leftBorder) {
-					j++;
-				}
-				if (j < positions.size() && positions.get(j) < rightBorder) {
-					leftBorder = positions.get(j);
-					selectedIndexes[i] = j;
-				} else {
-					while (j < positions.size() && positions.get(j) <= leftAfterRightBorder) {
-						j++;
-					}
-					if (j < positions.size()) {
-						leftAfterRightBorder = positions.get(j);
-						selectedIndexes[i] = j;
-					} else {
-						j--;
-						selectIndexes(selectedIndexes, charPositions, positions.get(j), i);
-						selectedIndexes[i] = j;
-					}
-				}
+    private void permute(final int i, final List<Integer> positions) {
+    	if (i == ALPHABER_SIZE) {
+    		final List<Integer> copiedPositions = new ArrayList<>(positions);
+    		Collections.sort(copiedPositions);
+    		final StringBuilder strBuilder = new StringBuilder();
+    		for (final Integer pos : copiedPositions) {
+    			strBuilder.append(input.charAt(pos));
+			}
+    		final String currString = strBuilder.toString();
+    		if (currString.compareTo(result) < 0) {
+    			result = currString;
+			}
+		} else if (charPositions[i].isEmpty()) {
+			permute(i + 1, positions);
+		} else {
+			for (final Integer charPosition : charPositions[i]) {
+				positions.add(charPosition);
+				permute(i + 1, positions);
+				positions.remove(positions.size() - 1);
 			}
 		}
-    	return selectedIndexes;
 	}
-    
+
     public static void main(final String[] args) {
         System.out.println("abc == " + new RemoveDuplicateLetters().removeDuplicateLetters("bcabc"));
         System.out.println("acdb == " + new RemoveDuplicateLetters().removeDuplicateLetters("cbacdcbc"));
