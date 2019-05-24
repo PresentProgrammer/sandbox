f705c48cac69e353594f3de0367056b2446a9e4f
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-05-24 21:25:22 +0300

    Solve RemoveDuplicateLetters recursively (after peeking).

--- Practice/src/main/java/RemoveDuplicateLetters.java
+++ Practice/src/main/java/RemoveDuplicateLetters.java
@@ -1,62 +1,48 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * Problem #316
- * Time complexity: O(?)
- * Space complexity: O(?)
+ * Time complexity: O(n)
+ * Space complexity: O(1)
  **/
 public class RemoveDuplicateLetters {
 
-	private static final String INITIAL_STRING = "{";
-    private static final int ALPHABER_SIZE = 26;
+    private static final int SIZE = 128;
 
-    private String input;
-    private String result;
-    private List<Integer>[] charPositions;
-
-    @SuppressWarnings("unchecked")
     public String removeDuplicateLetters(final String s) {
-    	input = s;
-    	result = INITIAL_STRING;
-		charPositions = new List[ALPHABER_SIZE];
-		for (int i = 0; i < ALPHABER_SIZE; i++) {
-		    charPositions[i] = new ArrayList<>();
-        }
-		for (int i = 0; i < s.length(); i++) {
-		    charPositions[s.charAt(i) - 'a'].add(i);
-        }
-		permute(0, new ArrayList<>());
-		return result.equals(INITIAL_STRING) ? "" : result;
-    }
-
-    private void permute(final int i, final List<Integer> positions) {
-    	if (i == ALPHABER_SIZE) {
-    		final List<Integer> copiedPositions = new ArrayList<>(positions);
-    		Collections.sort(copiedPositions);
-    		final StringBuilder strBuilder = new StringBuilder();
-    		for (final Integer pos : copiedPositions) {
-    			strBuilder.append(input.charAt(pos));
+    	if (s.isEmpty()) {
+    		return "";
+		}
+    	final int[] counts = new int[SIZE];
+    	for (int i = 0; i < s.length(); i++) {
+    		counts[s.charAt(i)]++;
+		}
+    	int minInd = 0;
+    	for (int i = 0; i < s.length(); i++) {
+    		if (s.charAt(i) < s.charAt(minInd)) {
+    			minInd = i;
 			}
-    		final String currString = strBuilder.toString();
-    		if (currString.compareTo(result) < 0) {
-    			result = currString;
-			}
-		} else if (charPositions[i].isEmpty()) {
-			permute(i + 1, positions);
-		} else {
-			for (final Integer charPosition : charPositions[i]) {
-				positions.add(charPosition);
-				permute(i + 1, positions);
-				positions.remove(positions.size() - 1);
+    		if (--counts[s.charAt(i)] == 0) {
+    			break;
 			}
 		}
+		return s.isEmpty() ? "" : s.charAt(minInd) + removeDuplicateLetters(removeAll(s.substring(minInd + 1), s.charAt(minInd)));
+	}
+
+	private static String removeAll(final String s, final char removed) {
+    	final StringBuilder builder = new StringBuilder();
+    	for (final char ch : s.toCharArray()) {
+    		if (ch != removed) {
+    			builder.append(ch);
+			}
+		}
+    	return builder.toString();
 	}
 
     public static void main(final String[] args) {
+        System.out.println("dcbax == " + new RemoveDuplicateLetters().removeDuplicateLetters("dcbaxabcd"));
         System.out.println("abc == " + new RemoveDuplicateLetters().removeDuplicateLetters("bcabc"));
         System.out.println("acdb == " + new RemoveDuplicateLetters().removeDuplicateLetters("cbacdcbc"));
         System.out.println("cadb == " + new RemoveDuplicateLetters().removeDuplicateLetters("cbaddabaa"));
+        System.out.println("hesitxyplovdqfkz == " + new RemoveDuplicateLetters().removeDuplicateLetters("thesqtitxyetpxloeevdeqifkz"));
+        System.out.println("bhcsdikworfltuzjxaympev == " + new RemoveDuplicateLetters().removeDuplicateLetters("bxshkpdwcsjdbikywvioxrypfzfbppydfilfxxtouzzjxaymjpmdoevv"));
 	}
 }
--- Practice/src/test/java/SomeTests.java
+++ Practice/src/test/java/SomeTests.java
@@ -2,16 +2,8 @@ import org.junit.Test;
 
 public class SomeTests {
 
-    private static final int[][] DIRECTIONS = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
     @Test
     public void m() {
-        for (int i = -10; i <= 10; i++) {
-            System.out.println(i + ", " + getDirectionIndex(i));
-        }
-    }
-
-    private int getDirectionIndex(final int direction) {
-        return (DIRECTIONS.length + direction % DIRECTIONS.length) % DIRECTIONS.length;
+        System.out.println("abacada".replaceAll("a", ""));
     }
 }
