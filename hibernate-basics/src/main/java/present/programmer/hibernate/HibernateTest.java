aa258d5e467af3aa9f1500219cec595170297b21
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-08-03 19:31:25 +0300

    Test IDEA working with Lombok Builders Lombok.

--- hibernate-basics/src/main/java/present/programmer/hibernate/HibernateTest.java
+++ hibernate-basics/src/main/java/present/programmer/hibernate/HibernateTest.java
@@ -4,16 +4,17 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import present.programmer.hibernate.domain.User;
+import present.programmer.hibernate.domain.User.UserBuilder;
 
 public class HibernateTest {
 
-	private static final int ID = 1;
+	private static final int ID = 2;
 
 	public static void main(String[] args) {
 		try (final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory()) {
 			try (final Session session = sessionFactory.openSession()) {
 				session.beginTransaction();
-				session.save(User.builder().id(ID).name("Peppa").build());
+				session.save(buildUser());
 				session.getTransaction().commit();
 			}
 			try (final Session session = sessionFactory.openSession()) {
@@ -21,5 +22,10 @@ public class HibernateTest {
 				System.out.println(retrievedUser.toString());
 			}
 		}
+	}
+
+	private static User buildUser() {
+		final UserBuilder builder = User.builder();
+		return builder.id(ID).name("Peppa").build();
 	}
 }
