f091bb30ce4b9597735a06049d2e1c5ede187094
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2019-10-14 00:47:48 +0300

    Solve #975 on LeetCode: fifth attempt, TreeMap.

--- Practice/src/main/java/OddEvenJump.java
+++ Practice/src/main/java/OddEvenJump.java
@@ -1,6 +1,5 @@
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -11,74 +10,26 @@ import java.util.TreeMap;
 public class OddEvenJump {
 
     public int oddEvenJumps(int[] arr) {
-        final int[] oddWhere = calcWhereToJump(arr, true);
-        final int[] evenWhere = calcWhereToJump(arr, false);
-
-        final Boolean[] oddReachable = new Boolean[arr.length];
-        oddReachable[oddReachable.length - 1] = true;
-        final Boolean[] evenReachable = Arrays.copyOf(oddReachable, oddReachable.length);
-        for (int i = 0; i < arr.length; i++) {
-            int currPos = i;
-            boolean currOdd = true;
-            final Deque<Integer> jumpsFrom = new ArrayDeque<>();
-            jumpsFrom.push(currPos);
-            Boolean isReachable = isReachable(currOdd, currPos, oddReachable, evenReachable, oddWhere, evenWhere);
-            while (isReachable == null) {
-                currPos = currOdd ? oddWhere[currPos] : evenWhere[currPos];
-                jumpsFrom.push(currPos);
-                currOdd = !currOdd;
-                isReachable = isReachable(currOdd, currPos, oddReachable, evenReachable, oddWhere, evenWhere);
-            }
-            while (!jumpsFrom.isEmpty()) {
-                final Boolean[] reachable = currOdd ? oddReachable : evenReachable;
-                reachable[jumpsFrom.pop()] = isReachable;
-                currOdd = !currOdd;
-            }
-        }
-
-        System.out.println(Arrays.toString(oddReachable));
-        int goodIndexes = 0;
-        for (final Boolean reachable : oddReachable) {
-            if (Boolean.TRUE.equals(reachable)) {
-                goodIndexes++;
-            }
-        }
-        return goodIndexes;
-    }
-
-    private static Boolean isReachable(final boolean currOdd, final int currPos,
-            final Boolean[] oddReachable, final Boolean[] evenReachable,
-            final int[] oddWhere, final int[] evenWhere) {
-        if (currOdd) {
-            if (oddReachable[currPos] == null) {
-                return oddWhere[currPos] >= 0 ? null : false;
-            } else {
-                return oddReachable[currPos];
-            }
-        } else {
-            if (evenReachable[currPos] == null) {
-                return evenWhere[currPos] >= 0 ? null : false;
-            } else {
-                return evenReachable[currPos];
-            }
-        }
-    }
-
-    private static int[] calcWhereToJump(final int[] arr, final boolean odd) {
-        final int[] result = new int[arr.length];
-        Arrays.fill(result, -1);
-        final TreeMap<Integer, Integer> valuePos = new TreeMap<>();
+        final boolean[] odd = new boolean[arr.length];
+        odd[odd.length - 1] = true;
+        final boolean[] even = Arrays.copyOf(odd, odd.length);
+        final TreeMap<Integer, Integer> pos = new TreeMap<>();
         for (int i = arr.length - 1; i >= 0; i--) {
-            final Integer identicalValue = valuePos.get(arr[i]);
-            if (identicalValue != null) {
-                result[i] = identicalValue;
-            } else {
-                final Integer closestKey = odd ? valuePos.higherKey(arr[i]) : valuePos.lowerKey(arr[i]);
-                if (closestKey != null) {
-                    result[i] = valuePos.get(closestKey);
-                }
+            final Map.Entry<Integer, Integer> ceiling = pos.ceilingEntry(arr[i]);
+            if (ceiling != null) {
+                odd[i] = even[ceiling.getValue()];
             }
-            valuePos.put(arr[i], i);
+            final Map.Entry<Integer, Integer> floor = pos.floorEntry(arr[i]);
+            if (floor != null) {
+                even[i] = odd[floor.getValue()];
+            }
+            pos.put(arr[i], i);
+        }
+        int result = 0;
+        for (final boolean oddElem : odd) {
+            if (oddElem) {
+                result++;
+            }
         }
         return result;
     }
