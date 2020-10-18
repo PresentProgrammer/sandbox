cc175e2ffd3eacbfc02dc47dc170578507729abc
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2020-10-18 18:49:44 +0300

    Refactor #493 Binary Search Tree solution.

--- Practice/src/main/java/ReversePairs_BinarySearchTree.java
+++ Practice/src/main/java/ReversePairs_BinarySearchTree.java
@@ -15,11 +15,11 @@ public class ReversePairs_BinarySearchTree {
             return 0;
         }
 
-        final Node root = toCustomBinarySearchTree(nums);
+        final CustomBinarySearchTree bst = new CustomBinarySearchTree(nums);
         int count = 0;
         for (final int num : nums) {
-            count += root.countGreater((long) num * 2);
-            root.insert(num);
+            count += bst.countGreater((long) num * 2);
+            bst.insert(num);
         }
         return count;
     }
@@ -36,51 +36,68 @@ public class ReversePairs_BinarySearchTree {
                         .toArray()));
     }
 
-    private static Node toCustomBinarySearchTree(int[] nums) {
-        final int[] arr = Arrays.stream(nums)
-                .sorted()
-                .distinct()
-                .toArray();
-        return toBST(arr, 0, arr.length - 1);
-    }
+    private static class CustomBinarySearchTree {
 
-    private static Node toBST(int[] arr, int start, int endIncl) {
-        if (start > endIncl) {
-            return null;
-        }
-        final int mid = (start + endIncl) >>> 1;
-        final Node root = new Node(arr[mid]);
-        root.left = toBST(arr, start, mid - 1);
-        root.right = toBST(arr, mid + 1, endIncl);
-        return root;
-    }
+        private final Node root;
 
-    private static class Node {
-        private final int val;
-        private int countGE;
-        private Node left;
-        private Node right;
-
-        Node(int val) {
-            this.val = val;
+        CustomBinarySearchTree(int[] nums) {
+            this.root = toCustomBinarySearchTree(nums);
         }
 
         void insert(int newVal) {
-            if (newVal >= this.val) {
-                this.countGE++;
-            }
-            if (newVal > this.val) {
-                this.right.insert(newVal);
-            } else if (newVal < this.val) {
-                this.left.insert(newVal);
-            }
+            root.insert(newVal);
         }
 
         int countGreater(long other) {
-            if (other < this.val) {
-                return this.countGE + (this.left == null ? 0 : this.left.countGreater(other));
-            } else {
-                return this.right == null ? 0 : this.right.countGreater(other);
+            return root.countGreater(other);
+        }
+
+        private static Node toCustomBinarySearchTree(int[] nums) {
+            final int[] arr = Arrays.stream(nums)
+                    .sorted()
+                    .distinct()
+                    .toArray();
+            return toBST(arr, 0, arr.length - 1);
+        }
+
+        private static Node toBST(int[] arr, int start, int endIncl) {
+            if (start > endIncl) {
+                return null;
+            }
+            final int mid = (start + endIncl) >>> 1;
+            final Node root = new Node(arr[mid]);
+            root.left = toBST(arr, start, mid - 1);
+            root.right = toBST(arr, mid + 1, endIncl);
+            return root;
+        }
+
+        private static class Node {
+            private final int val;
+            private int countGE;
+            private Node left;
+            private Node right;
+
+            Node(int val) {
+                this.val = val;
+            }
+
+            void insert(int newVal) {
+                if (newVal >= this.val) {
+                    countGE++;
+                }
+                if (newVal > this.val) {
+                    right.insert(newVal);
+                } else if (newVal < this.val) {
+                    left.insert(newVal);
+                }
+            }
+
+            int countGreater(long other) {
+                if (other < this.val) {
+                    return countGE + (left == null ? 0 : left.countGreater(other));
+                } else {
+                    return right == null ? 0 : right.countGreater(other);
+                }
             }
         }
     }
