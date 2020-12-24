783b90796ef92f985b3f7ff547425a7504209b0b
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2020-12-24 22:06:41 +0200

    Eulerian Path application.

---
+++ Practice/src/main/java/CrackingTheSafe.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem #753
+ * Solved with Eulerian Path approach.
+ * Time complexity: O(|E|) = O(k ^ n), as |V| = k ^ (n - 1), and |E| = |V| * k
+ * Space complexity: O(|V| + |E|) = O(k ^ n)
+ **/
+public class CrackingTheSafe {
+
+    private final Map<String, Integer> nextEdges = new HashMap<>();
+    private final StringBuilder resultBuilder = new StringBuilder();
+    private int k;
+
+    public String crackSafe(int n, int k) {
+        this.k = k;
+        final String start = createStart(n);
+        dfs(start);
+        return resultBuilder.append(start).toString();
+    }
+
+    private void dfs(String curr) {
+        int nextEdge;
+        while ((nextEdge = nextEdges.computeIfAbsent(curr, unusedKey -> 0)) < k) {
+            nextEdges.put(curr, nextEdge + 1);
+            dfs((curr + nextEdge).substring(1));
+            resultBuilder.append(nextEdge);
+        }
+    }
+
+    private static String createStart(int n) {
+        final StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < n - 1; i++) {
+            strBuilder.append('0');
+        }
+        return strBuilder.toString();
+    }
+    
+    public static void main(final String[] args) {
+        System.out.println("01100 == " + new CrackingTheSafe().crackSafe(2, 2));
+	}
+}
