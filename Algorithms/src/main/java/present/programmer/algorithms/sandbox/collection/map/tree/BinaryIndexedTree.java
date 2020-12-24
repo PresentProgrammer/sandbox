f1b6516c77504d08f767553a9614117dd7a17768
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2020-12-24 17:51:48 +0200

    Binary Indexed Tree reference.

---
+++ Algorithms/src/main/java/present/programmer/algorithms/sandbox/collection/map/tree/BinaryIndexedTree.java
@@ -0,0 +1,13 @@
+package present.programmer.algorithms.sandbox.collection.map.tree;
+
+/**
+ * https://www.topcoder.com/community/competitive-programming/tutorials/binary-indexed-trees/
+ *
+ * Consider the following problem: There are n boxes that undergo the following queries:
+ * 1. add marble to box i
+ * 2. sum marbles from box k to box l
+ * We have N elements and M queries.
+ * This data structure allows to solve the problem with time complexity O(M log N) (alternatively, Red-Black BST could be used, but implementation of this is much simpler shorter).
+ */
+public class BinaryIndexedTree {
+}
