a6edd83a3206b4ed50de58727f97d19ea09cc732
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2021-01-16 18:32:27 +0200

    Solve #973 on LeetCode, with Max Size PQ O(N log K).

--- Practice/src/main/java/KClosestPointsToOrigin.java
+++ Practice/src/main/java/KClosestPointsToOrigin.java
@@ -1,71 +1,51 @@
 import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Problem #973
- * Time complexity: O(N)
- * Space complexity: O(N)
+ * Time complexity: O(N log K)
+ * Space complexity: O(K)
  **/
 public class KClosestPointsToOrigin {
 
     public int[][] kClosest(int[][] points, int K) {
-        final int[] distances = Arrays.stream(points)
-                .mapToInt(p -> p[0] * p[0] + p[1] * p[1])
-                .toArray();
-        final int[] indexes = IntStream.range(0, distances.length).toArray();
-        quickSelect(indexes, distances, K - 1);
-        return IntStream.of(indexes)
-                .limit(K)
-                .mapToObj(i -> points[i])
+        final MaxSizePQ pq = new MaxSizePQ(K);
+        for (final int[] point : points) {
+            pq.offer(new Point(point));
+        }
+        return pq.stream()
+                .map(p -> p.coordinates)
                 .toArray(int[][]::new);
     }
 
-    private static void quickSelect(int[] indexes, int[] distances, int k) {
-        shuffle(indexes);
-        int lo = 0;
-        int hi = indexes.length - 1;
-        int j = hi;
-        while (j != k) {
-            j = partition(indexes, distances, lo, hi);
-            if (j > k) {
-                hi = j - 1;
-            } else if (j < k) {
-                lo = j + 1;
-            }
+    private static class Point {
+        final int[] coordinates;
+        final int distance;
+
+        Point(int[] coordinates) {
+            this.coordinates = Arrays.copyOf(coordinates, coordinates.length);
+            this.distance = coordinates[0] * coordinates[0] + coordinates[1] * coordinates[1];
         }
     }
 
-    private static int partition(int[] indexes, int[] distances, int lo, int hi) {
-        int i = lo + 1;
-        int j = hi;
-        while (true) {
-            while (i <= hi && distances[indexes[i]] < distances[indexes[lo]]) {
-                i++;
-            }
-            while (distances[indexes[j]] > distances[indexes[lo]]) {
-                j--;
-            }
-            if (i < j) {
-                swap(indexes, i++, j--);
-            } else {
-                swap(indexes, lo, j);
-                return j;
-            }
-        }
-    }
+    private static class MaxSizePQ extends PriorityQueue<Point> {
 
-    private static void shuffle(int[] arr) {
-        final Random rand = new Random();
-        for (int i = 1; i < arr.length; i++) {
-            swap(arr, i, rand.nextInt(i + 1));
-        }
-    }
+        private final int maxSize;
 
-    private static void swap(int[] arr, int a, int b) {
-        final int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+        MaxSizePQ(int maxSize) {
+            super(maxSize + 1, Comparator.comparingInt((Point p) -> p.distance).reversed());
+            this.maxSize = maxSize;
+        }
+
+        @Override
+        public boolean offer(Point p) {
+            super.offer(p);
+            if (size() > maxSize) {
+                poll();
+            }
+            return true;
+        }
     }
 
     public static void main(final String[] args) {
