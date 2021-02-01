a40460e09f8bd2a19e023b82db29acaa2a86c150
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2021-02-01 23:59:11 +0200

    Re-solve Subsets, #78.

--- Practice/src/main/java/Subsets.java
+++ Practice/src/main/java/Subsets.java
@@ -9,18 +9,17 @@ import java.util.List;
 public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
-        final List<List<Integer>> result = new ArrayList<>();
-        subsets(result, new ArrayList<>(), nums, 0);
-        return result;
+        return subsets(nums, new ArrayList<>(), new ArrayList<>(), 0);
     }
 
-    private static void subsets(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+    private static List<List<Integer>> subsets(int[] nums, List<Integer> temp, List<List<Integer>> result, int firstToUse) {
         result.add(new ArrayList<>(temp));
-        for (int i = start; i < nums.length; i++) {
+        for (int i = firstToUse; i < nums.length; i++) {
             temp.add(nums[i]);
-            subsets(result, temp, nums, i + 1);
+            subsets(nums, temp, result, i + 1);
             temp.remove(temp.size() - 1);
         }
+        return result;
     }
 
     public static void main(final String[] args) {
