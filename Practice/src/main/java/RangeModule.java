3ca065254483141f1e2759ed7ff92fd870c3d6cb
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2021-02-17 00:29:29 +0200

    Solve #715.

---
+++ Practice/src/main/java/RangeModule.java
@@ -0,0 +1,68 @@
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+/**
+ * Problem #715
+ * Time complexity: O((A + R) * log(A + R) + Q * log(A + R)), where A - add, R - remove, Q - query operation count.
+ * Space complexity: O(A + R)
+ **/
+public class RangeModule {
+
+    private static final boolean START = true;
+    private static final boolean END = false;
+
+    private final TreeMap<Integer, Boolean> treeMap = new TreeMap<>();
+
+    public void addRange(int left, int right) {
+        treeMap.subMap(left, right).clear();
+
+        final Entry<Integer, Boolean> lowerEntry = treeMap.lowerEntry(left);
+        if (lowerEntry == null || lowerEntry.getValue() == END) {
+            treeMap.put(left, START);
+        }
+
+        final Boolean rightVal = treeMap.get(right);
+        if (rightVal == null) {
+            final Entry<Integer, Boolean> higherEntry = treeMap.higherEntry(right);
+            if (higherEntry == null || higherEntry.getValue() == START) {
+                treeMap.put(right, END);
+            }
+        } else if (rightVal == START) {
+            treeMap.remove(right);
+        }
+    }
+
+    public boolean queryRange(int left, int right) {
+        final Entry<Integer, Boolean> floorEntry = treeMap.floorEntry(left);
+        return floorEntry != null && floorEntry.getValue() == START &&
+                floorEntry.getKey().equals(treeMap.lowerKey(right));
+    }
+
+    public void removeRange(int left, int right) {
+        treeMap.subMap(left, right).clear();
+
+        final Entry<Integer, Boolean> lowerEntry = treeMap.lowerEntry(left);
+        if (lowerEntry != null && lowerEntry.getValue() == START) {
+            treeMap.put(left, END);
+        }
+
+        final Boolean rightVal = treeMap.get(right);
+        if (rightVal == null) {
+            final Entry<Integer, Boolean> higherEntry = treeMap.higherEntry(right);
+            if (higherEntry != null && higherEntry.getValue() == END) {
+                treeMap.put(right, START);
+            }
+        } else if (rightVal == END) {
+            treeMap.remove(right);
+        }
+    }
+
+    public static void main(final String[] args) {
+        final RangeModule rm = new RangeModule();
+        rm.addRange(10, 20);
+        rm.removeRange(14, 16);
+        System.out.println("true == " + rm.queryRange(10, 14));
+        System.out.println("true == " + rm.queryRange(16, 17));
+        System.out.println("false == " + rm.queryRange(13, 15));
+    }
+}
--- Practice/src/test/java/SomeTests.java
+++ Practice/src/test/java/SomeTests.java
@@ -1,13 +1,17 @@
 import org.junit.Test;
 
+import java.util.TreeMap;
+
 public class SomeTests {
 
     @Test
-    public void m() {
-        final String str = new StringBuilder()
-                .append('\\')
-                .append("\\")
-                .toString();
-        System.out.println(str);
+    public void treeMap_subMap_clear() {
+        final TreeMap<Integer, Boolean> treeMap = new TreeMap<>();
+        treeMap.put(1, true);
+        treeMap.put(4, true);
+        treeMap.put(6, true);
+        treeMap.put(10, true);
+        treeMap.subMap(4, 7).clear();
+        System.out.println(treeMap);
     }
 }
