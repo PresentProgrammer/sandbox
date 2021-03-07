2d36425416522f90e383362fd39a6dda5b5378e2
PresentProgrammer <Arturs.Solomencevs@gmail.com>
2021-03-07 20:41:51 +0200

    Re-solved #1153

--- Practice/src/main/java/StringTransformsIntoAnotherString.java
+++ Practice/src/main/java/StringTransformsIntoAnotherString.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 /**
  * Problem #1153
@@ -8,31 +7,47 @@ import java.util.stream.IntStream;
  **/
 public class StringTransformsIntoAnotherString {
 
-    public boolean canConvert(String str1, String str2) {
-        if (str1.equals(str2)) {
-            return true;
-        }
+    private static final int EMPTY = -1;
+    private static final int ABC_LENGTH = 'z' - 'a' + 1;
 
-        final int[] map = new int['z' - 'a' + 1];
-        Arrays.fill(map, -1);
+    public boolean canConvert(String str1, String str2) {
+        final int[] map = new int[ABC_LENGTH];
+        Arrays.fill(map, EMPTY);
         for (int i = 0; i < str1.length(); i++) {
-            final int char1 = str1.charAt(i) - 'a';
-            final int char2 = str2.charAt(i) - 'a';
+            final int char1 = toInt(str1.charAt(i));
+            final int char2 = toInt(str2.charAt(i));
             if (map[char1] >= 0 && map[char1] != char2) {
                 return false;
             } else {
                 map[char1] = char2;
             }
         }
-        return isFreeCharacterInValues(map);
+        return isFreeCharacterInValues(map) || allMatch(map);
     }
 
     private static boolean isFreeCharacterInValues(int[] map) {
-        return IntStream.of(map).sum() != arithmeticProgressionSum();
+        final boolean[] used = new boolean[ABC_LENGTH];
+        for (final int value : map) {
+            if (value == EMPTY || used[value]) {
+                return true;
+            } else {
+                used[value] = true;
+            }
+        }
+        return false;
     }
 
-    private static int arithmeticProgressionSum() {
-        return ('z' - 'a') * ('z' - 'a' + 1) / 2;
+    private static boolean allMatch(int[] map) {
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int toInt(char ch) {
+        return ch - 'a';
     }
 
     public static void main(final String[] args) {
