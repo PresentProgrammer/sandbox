3417c1aac79e8ce9c99e246b344bf29d6ee56a1a
Arturs Solomencevs <arturs.solomencevs@omniva.ee>
2022-03-11 11:54:03 +0200

    JavaScript basics: use scripts.

--- JavaScriptBasics/src/main/webapp/documents/npm-webpack-babel/index.js
+++ JavaScriptBasics/src/main/webapp/documents/npm-webpack-babel/index.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
 
 console.log(document.getElementById('results'));
-const msg = `Hello with webpack.config.js and babel! ${moment().startOf('day').fromNow()}`;
+const msg = `Hello with webpack.config.js, babel and scripts! ${moment().startOf('day').fromNow()}`;
 document.getElementById('results').innerHTML = msg;
 
-const name = "Bob", time = "today";
+const name = "Arturs", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
